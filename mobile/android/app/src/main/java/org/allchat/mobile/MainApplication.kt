804ca214a87a54e1edea55bfcdb23e94fc8ad0a1
Ilya Petrov <ilyapetrov.mosobl@gmail.com>
2026-08-16 17:20:37 +0300

    fix(android): register SVG native package

--- mobile/android/app/src/main/java/org/allchat/mobile/MainApplication.kt
+++ mobile/android/app/src/main/java/org/allchat/mobile/MainApplication.kt
@@ -6,6 +6,7 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.horcrux.svg.SvgPackage
 import com.oney.WebRTCModule.WebRTCModuleOptions
 import org.webrtc.PeerConnectionFactory
 import org.webrtc.audio.JavaAudioDeviceModule
@@ -17,6 +18,7 @@ class MainApplication : Application(), ReactApplication {
       context = applicationContext,
       packageList =
         PackageList(this).packages.apply {
+          add(SvgPackage())
           add(AllChatAudioPackage())
           add(AllChatMediaSaverPackage())
           add(AllChatUpdaterPackage())

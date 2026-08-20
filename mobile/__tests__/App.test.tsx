7bdd27d4dca30ab2a54f9ed9f99468b0054204ca
Ilya Petrov <ilyapetrov.mosobl@gmail.com>
2026-08-20 11:45:38 +0300

    Keep incoming call modal below system bar

--- mobile/__tests__/App.test.tsx
+++ mobile/__tests__/App.test.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import App, {globalCallTopPadding} from '../App';
+import App, {incomingCallModalStatusBarTranslucent} from '../App';
 import {MemorySessionVault} from '../src/session/SessionVault';
 
 test('renders correctly', async () => {
@@ -14,5 +14,5 @@ test('renders correctly', async () => {
 });
 
 test('positions the incoming Call banner below the system status bar', () => {
-  expect(globalCallTopPadding(24)).toBe(32);
+  expect(incomingCallModalStatusBarTranslucent).toBe(false);
 });
